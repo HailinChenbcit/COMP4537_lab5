7b2f8e6248d6418a49c4d4308c68e25c75c29c6e
hailin chen <hchen256@my.bcit.ca>
2025-02-13 09:07:36 -0800

    Updated messages

--- en/en.js
+++ en/en.js
@@ -8,7 +8,7 @@ module.exports = {
     200: "<p style='color: green;'>Success</p>",
     400: "<p style='color: red;'>Invalid table name. Only 'patients' is allowed.</p>",
     404: "<p style='color: red;'>404 - Not Found</p>",
-    405: "'<p style='color: red;'>Method Not Allowed.</p>",
+    405: "<p style='color: red;'>Method Not Allowed.</p>",
     500: "<p style='color: red;'>Internal Error</p>",
   },
 };
